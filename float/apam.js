052b0c92f88b3cb773dca91250c376cf763d5b66
pgpump <pg079898@gmail.com>
2025-05-19 22:30:53 +0700

    Create apam.js

---
+++ float/apam.js
@@ -0,0 +1,14 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
+
+atOptions = {
+    'key': '4176be5ee66193fc3cff3d0840f8e95c',
+    'format': 'iframe',
+    'height': 90,
+    'width': 728,
+    'params': {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/4176be5ee66193fc3cff3d0840f8e95c/invoke.js"></scr' + 'ipt>');
+
+document.write('</div>');
+document.write('</div>');
